554ed6eab2fd9b3cbfab1eb33b5984a322a5c951
Benjamin Milde <benni@kobrakai.de>
2018-11-02 09:57:17 +0100

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FilterQueryParser.MixProject do
   def project do
     [
       app: :filter_query_parser,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
